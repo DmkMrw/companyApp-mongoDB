8bc0201b1b3c0eac88a511f450d489475541762f
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-25 14:20:56 +0200

    Add test for department endpoints

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -3,48 +3,48 @@ const expect = require('chai').expect;
 const mongoose = require('mongoose');
 
 
-// describe('Department', () => {
+describe('Department', () => {
 
-//   it('should throw an error if no "name" arg', () => {
-//     const dep = new Department({}); // create new Department, but don't set `name` attr value
+  it('should throw an error if no "name" arg', () => {
+    const dep = new Department({}); // create new Department, but don't set `name` attr value
 
-//     dep.validate(err => {
-//       expect(err.errors.name).to.exist;
-//     });
-//   });
+    dep.validate(err => {
+      expect(err.errors.name).to.exist;
+    });
+  });
 
-//   it('should throw an error if "name" is not a string', () => {
-//   const cases = [{}, []];
-//   for(let name of cases) {
-//     const dep = new Department({ name });
+  it('should throw an error if "name" is not a string', () => {
+  const cases = [{}, []];
+  for(let name of cases) {
+    const dep = new Department({ name });
 
-//       dep.validate(err => {
-//         expect(err.errors.name).to.exist;
-//       });
-//     }
-//   });
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
 
-//   it('should throw an error if "name" length is higher than 20 or lower than 5', () => {
+  it('should throw an error if "name" length is higher than 20 or lower than 5', () => {
 
-//     const cases = [ 'abc', 'abcdefghijklmnoprstuw'];
-//     for(let name of cases) {
-//       const dep = new Department({ name });
+    const cases = [ 'abc', 'abcdefghijklmnoprstuw'];
+    for(let name of cases) {
+      const dep = new Department({ name });
 
-//       dep.validate(err => {
-//         expect(err.errors.name).to.exist;
-//       });
-//     }
-//   });
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
 
-//   it('should render correct if agr is prop', () => {
+  it('should render correct if agr is prop', () => {
 
-//     const cases = [ 'Lorem ipsum', 'Lorem ipsum dolor'];
-//     for(let name of cases) {
-//       const dep = new Department({ name });
+    const cases = [ 'Lorem ipsum', 'Lorem ipsum dolor'];
+    for(let name of cases) {
+      const dep = new Department({ name });
 
-//       dep.validate(err => {
-//         expect(err).to.not.exist;
-//       });
-//     }
-//   });
-// });
+      dep.validate(err => {
+        expect(err).to.not.exist;
+      });
+    }
+  });
+});
